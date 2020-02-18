1b09415557372d2681252fad8648ddc23e6a7da7
TIGERCHANG123 <2462924814@qq.com>
2020-02-18 15:11:11 +0800

    infoGAN

--- infoGAN.py
+++ infoGAN.py
@@ -30,29 +30,40 @@ class generator(tf.keras.Model):
 class discriminator(tf.keras.Model):
   def __init__(self, input_shape):
     super(discriminator, self).__init__()
-    self.conv_input = layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',input_shape=input_shape)
-    self.bn1 = tf.keras.layers.BatchNormalization()
-    self.leakyRelu1 = layers.LeakyReLU(0.2)
-    self.dropout1 = layers.Dropout(0.3)
+    self.conv1=tf.keras.layers.Conv2D(64, kernel_size=5, strides=2, input_shape=input_shape, padding="same")
+    self.leakyRelu1=tf.keras.layers.LeakyReLU(alpha=0.2)
+    self.dropout1=tf.keras.layers.Dropout(0.25)
 
-    self.conv2 = layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same')
-    self.bn2 = tf.keras.layers.BatchNormalization()
-    self.leakyRelu2 = layers.LeakyReLU(0.2)
-    self.dropout2 = layers.Dropout(0.3)
-    self.flatten = layers.Flatten()
-    self.denseD = layers.Dense(1)
+    self.conv2=tf.keras.layers.Conv2D(128, kernel_size=5, strides=2, padding="same")
+    self.bn2=tf.keras.layers.BatchNormalization(momentum=0.8)
+    self.leakyRelu2=tf.keras.layers.LeakyReLU(alpha=0.2)
+    self.dropout2=tf.keras.layers.Dropout(0.25)
+
+    self.conv3=tf.keras.layers.Conv2D(256, kernel_size=5, strides=1, padding="same")
+    self.bn3=tf.keras.layers.BatchNormalization(momentum=0.8)
+    self.leakyRelu3=tf.keras.layers.LeakyReLU(alpha=0.2)
+    self.dropout3=tf.keras.layers.Dropout(0.25)
+
+    self.flatten4=tf.keras.layers.Flatten()
+    self.dense4=tf.keras.layers.Dense(1)
+
   def call(self, x):
-    x = self.conv_input(x)
-    x = self.bn1(x)
+    x = self.conv1(x)
     x = self.leakyRelu1(x)
     x = self.dropout1(x)
+
     x = self.conv2(x)
     x = self.bn2(x)
     x = self.leakyRelu2(x)
     x = self.dropout2(x)
-    x = self.flatten(x)
 
-    y = self.denseD(x)
+    x = self.conv3(x)
+    x = self.bn3(x)
+    x = self.leakyRelu3(x)
+    x = self.dropout3(x)
+
+    x = self.flatten4(x)
+    y = self.dense4(x)
     return x, y
 
 class auxiliary(tf.keras.Model):
@@ -72,7 +83,7 @@ class auxiliary(tf.keras.Model):
 
 def get_gan(gen_input_shape, img_shape):
   Generator = generator(gen_input_shape, img_shape)
-  Discriminator = discriminator(img_shape)
+  Discriminator = discriminator(input_shape=img_shape)
   Auxiliary = auxiliary()
   gen_name = 'infoGAN'
   return Generator, Discriminator, Auxiliary, gen_name
